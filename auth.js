21bb6ff5c4bcfbe343b8b97589a7f283fca098cf
Alex Donley <donleyac@gmail.com>
2018-01-22 17:14:43 -0500

    Added JWT authentication with local

--- auth.js
+++ auth.js
@@ -1,16 +1,44 @@
 import passport from 'passport';
-import {Strategy} from 'passport-http-bearer';
-import {findByToken} from './data/tokenStore.js';
+import {Strategy as LocalStrategy} from 'passport-local';
+import jwt from 'jsonwebtoken';
 
-// The Bearer strategy requires a `verify` function which receives the
-// credentials (`token`) contained in the request.  The function must invoke
-// `cb` with a user object, which will be set at `req.user` in route handlers
-// after authentication.
-passport.use(new Strategy(
-  function(token, cb) {
-    findByToken(token, function(err, user) {
+var records = [
+  {id: 1, username: 'alex', email:'donleyac@gmail.com', password: 'password'},
+  {id: 2, username: 'john', email:'john@gmail.com', password:'secure'}
+];
+// Local Strategy, used to initially get token
+passport.use(new LocalStrategy(
+  function(username, password, cb) {
+    findByCreds(username, password, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       return cb(null, user);
     });
   }));
+//Required because passport will not call default defaul when session:false
+export function serializeUser(req, res, next) {
+  req.user = {
+    id: req.user.id
+  };
+  next();
+}
+export function findByCreds(username, password, cb) {
+  // database dummy - find user and verify password
+  for(let i=0; i<records.length; i++) {
+    if(records[i]['username']===username && records[i]['password']===password) {
+      cb(null, records[i]);
+    }
+  }
+  cb(null, false);
+}
+export function generateToken(req, res, next) {
+  //expiresIn seconds
+  req.token = jwt.sign({id: req.user.id}, 'CHANGEME', {expiresIn: 120*60});
+  next();
+}
+export function localResponse(req, res) {
+  res.status(200).json({
+    user: req.user,
+    token: req.token
+  });
+}
--- server.js
+++ server.js
@@ -5,32 +5,29 @@ import schema from './data/schema';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import passport from 'passport';
-import session from 'express-session';
-import uuid from 'node-uuid';
-
-require('./auth.js');
+import {serializeUser, generateToken, localResponse} from './auth.js';
+import expressJwt from 'express-jwt';
 
 const app = express();
 const PORT = 3000;
-
-//passport's session piggy-backs on express-session
-app.use(session({
-  genid: function(req) {
-   return uuid.v4();
-  },
-  secret: 'CHANGEME'
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+const authenticateJwt = expressJwt({secret: 'CHANGEME'});
 //Security middleware that prevents certain types of requests
 app.use(helmet());
 //Logs all http requests to console
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }) );
+app.use(passport.initialize());
+app.post('/auth',
+  //Strategy verifies username/password in db returns User object
+  //serializeUser searches db for user and stores id in request object
+  //generateToken adds jwt tokent to request object
+  //localResponse responds with modified request object
+  passport.authenticate('local', {session: false}),
+    serializeUser, generateToken, localResponse);
 // curl -v -H "Authorization: Bearer 123456789" http://127.0.0.1:3000/
 // curl -v http://127.0.0.1:3000/?access_token=123456789
 app.use('/graphql',
-  passport.authenticate('bearer', { session: false }),
+  authenticateJwt,
   graphqlExpress({ schema })
 );
 app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
